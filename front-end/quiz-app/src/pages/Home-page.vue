3eb3d6ed931575bd2295b3abde06346f348fcf8a
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-02 14:45:22 +0200

    chore(frontend): Creating pages

---
+++ front-end/quiz-app/src/pages/Home-page.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h2>Play a quiz</h2>
+        <button class="button-big">Host lobby</button>
+        <button class="button-big">Join lobby</button>
+    </div>
+</template>
+<script>
+</script>
