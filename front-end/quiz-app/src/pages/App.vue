1f7d7e35e00a3d5937df0888085f048c05474327
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-02 14:44:53 +0200

    feat(frontend): Creating navigation on app page

---
+++ front-end/quiz-app/src/pages/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <nav>
+    <router-link to="/" class="nav-link">
+      <img src="../assets/icons/Icon awesome-arrow-alt-circle-left.svg" alt="Back" class="icon" />
+    </router-link>
+    
+    <router-link to="/" class="logo">
+      <h1>FlashQuiz</h1>
+    </router-link>
+
+    <router-link to="/profile" class="nav-link">
+      <img src="../assets/icons/Icon awesome-user-circle.svg" alt="Profile" class="icon" />
+    </router-link>
+  </nav>
+
+  <RouterView></RouterView>
+
+  <div id="app">
+    <div class="background-image"></div>
+    <footer>
+    </footer>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+  
+</style>
