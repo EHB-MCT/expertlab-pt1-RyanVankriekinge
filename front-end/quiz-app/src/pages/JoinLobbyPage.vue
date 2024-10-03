f62a76813befce580cde686026f13da54860cf6e
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-03 11:19:09 +0200

    feat(frontend): Creating join lobby page with input form to fill in lobby code

--- front-end/quiz-app/src/pages/JoinLobbyPage.vue
+++ front-end/quiz-app/src/pages/JoinLobbyPage.vue
@@ -1,8 +1,12 @@
 <template>
     <main>
-        <div class="content join-lobby">
-
-        </div>
+      <div class="content join-lobby">
+        <h2>Enter Lobby Code</h2>
+        <form class="join-lobby-form">
+          <input type="text" class="lobby-input input-field" placeholder="Enter lobby code" required/>
+          <button class="button-small" type="submit">Join</button>
+        </form>
+      </div>
     </main>
 </template>
 
