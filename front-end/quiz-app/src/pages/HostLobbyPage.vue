c436364c22d539b0ac16f683ea231b96adfa7e29
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-03 11:18:46 +0200

    feat(frontend): Creating host lobby page with a dropdown menu to choose quiz

--- front-end/quiz-app/src/pages/HostLobbyPage.vue
+++ front-end/quiz-app/src/pages/HostLobbyPage.vue
@@ -1,7 +1,17 @@
 <template>
     <main>
         <div class="content host-lobby">
+            <h2>Choose quiz</h2>
+            <form @submit.prevent="submitForm" class="choose-quiz">
+                <select v-model="selectedQuiz" class="quiz-dropdown input-field">
+                    <option disabled value="">Select a quiz</option>
+                    <option value="quiz1">Quiz 1</option>
+                    <option value="quiz2">Quiz 2</option>
+                    <option value="quiz3">Quiz 3</option>
+                </select>
 
+                <button class="button-small" type="submit">Continue</button>
+            </form>
         </div>
     </main>
 </template>
