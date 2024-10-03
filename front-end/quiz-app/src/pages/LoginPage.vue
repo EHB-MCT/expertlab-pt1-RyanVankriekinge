b028808a4e32a1c38053dced7538c033d8a78469
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-03 11:36:45 +0200

    chore(frontend): Create login page

---
+++ front-end/quiz-app/src/pages/LoginPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <main>
+        <div class="content login">
+            <h2>Create account</h2>
+
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'LoginPage',
+    };
+</script>
