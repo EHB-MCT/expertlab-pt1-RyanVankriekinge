0d7a8c89ab5650e065dcfcdf6579237750aeb0bc
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-08 09:31:44 +0200

    feat(frontend):  added template for create-account-page

--- front-end/quiz-app/src/pages/CreateAccountPage.vue
+++ front-end/quiz-app/src/pages/CreateAccountPage.vue
@@ -1,9 +1,44 @@
 <template>
     <main>
-        <div class="content create-account">
-            <h2>Create account</h2>
+      <div class="content create-account">
+        <h2>Create account</h2>
+        <form @submit.prevent="join" class="create-account-form">
+          <input
+            v-model="username"
+            type="text"
+            class="input-field"
+            placeholder="Username"
+            required
+          />
 
-        </div>
+          <input
+            v-model="email"
+            type="email"
+            class="input-field"
+            placeholder="Email"
+            required
+          />
+
+          <input
+            v-model="password"
+            type="password"
+            class="input-field"
+            placeholder="Password"
+            required
+          />
+
+          <input
+            v-model="confirmPassword"
+            type="password"
+            class="input-field"
+            placeholder="Confirm Password"
+            required
+          />
+
+          <button class="button-small" type="submit">Confirm</button>
+        </form>
+        <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+      </div>
     </main>
 </template>
 
