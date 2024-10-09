67616e34a77b1be1b0e92c2fc567ace2bc7205a0
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-09 10:30:15 +0200

    feat(frontend): loading user quizzes in drop down menu when choosing a quiz to host

--- front-end/quiz-app/src/pages/CreateLobbyPage.vue
+++ front-end/quiz-app/src/pages/CreateLobbyPage.vue
@@ -2,14 +2,13 @@
     <main>
         <div class="content create-lobby">
             <h2>Choose quiz</h2>
-            <form @submit.prevent="submitForm" class="choose-quiz">
+            <form @submit.prevent="submitChooseQuizForm" class="choose-quiz">
                 <select v-model="selectedQuiz" class="quiz-dropdown input-field">
                     <option disabled value="">Select a quiz</option>
-                    <option value="quiz1">Quiz 1</option>
-                    <option value="quiz2">Quiz 2</option>
-                    <option value="quiz3">Quiz 3</option>
+                    <option v-for="quiz in quizzes" :key="quiz._id" :value="quiz._id">
+                        {{ quiz.title }}
+                    </option>
                 </select>
-
                 <button class="button-small" type="submit">Continue</button>
             </form>
         </div>
@@ -19,5 +18,40 @@
 <script>
     export default {
         name: 'CreateLobbyPage',
+        data() {
+            return {
+                selectedQuiz: '', 
+                quizzes: [], 
+                errorMessage: '' 
+            };
+        },
+        mounted() {
+            this.fetchUserQuizzes(); 
+        },
+        methods: {
+            async fetchUserQuizzes() {
+                try {
+                    const response = await fetch('http://localhost:3000/user-quizzes', {
+                        method: 'GET',
+                        credentials: 'include' 
+                    });
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+
+                    const data = await response.json();
+                    console.log('Fetched quizzes:', data); 
+                    if (data.quizzes && Array.isArray(data.quizzes)) {
+                        this.quizzes = data.quizzes; 
+                    } else {
+                        this.errorMessage = 'Failed to fetch quizzes';
+                        console.error(this.errorMessage);
+                    }
+                } catch (error) {
+                    this.errorMessage = 'An error occurred while fetching quizzes';
+                    console.error(this.errorMessage, error);
+                }
+            }
+        }
     };
 </script>
