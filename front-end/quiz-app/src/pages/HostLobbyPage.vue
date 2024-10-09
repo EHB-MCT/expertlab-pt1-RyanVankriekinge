b3addf90b9892389593ea057973e6074d11e2cad
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-09 15:22:43 +0200

    feat(frontend): creating template for the lobby (pov from host)

---
+++ front-end/quiz-app/src/pages/HostLobbyPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <main>
+      <div class="content join-lobby">
+        <h2>Enter Lobby Code</h2>
+        <form class="join-lobby-form">
+            <input type="text" class="display-field" value="XXXX" readonly>
+            <p style="margin-bottom: 30px; margin-top: 0px;">Share this room code with your friends!</p>
+          <button class="button-small" type="submit">Start Quiz</button>
+          <p>../10 players joined</p>
+        </form>
+      </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'HostLobbyPage',
+    };
+</script>
