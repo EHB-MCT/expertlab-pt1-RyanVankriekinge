890a15ad276c2ca3e1c4c1d1df7322f28c6a0585
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-09 15:24:55 +0200

    fix(frontend): changing title on lobby page to 'lobby code'

--- front-end/quiz-app/src/pages/HostLobbyPage.vue
+++ front-end/quiz-app/src/pages/HostLobbyPage.vue
@@ -1,7 +1,7 @@
 <template>
     <main>
       <div class="content join-lobby">
-        <h2>Enter Lobby Code</h2>
+        <h2>Lobby Code</h2>
         <form class="join-lobby-form">
             <input type="text" class="display-field" value="XXXX" readonly>
             <p style="margin-bottom: 30px; margin-top: 0px;">Share this room code with your friends!</p>
