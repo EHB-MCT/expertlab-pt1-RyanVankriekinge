bedffdad93a25bac129534ef6ced9bb93d9748af
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-10 09:51:35 +0200

    feat(frontend): adding data and event-listener for lobby joining

--- front-end/quiz-app/src/pages/JoinLobbyPage.vue
+++ front-end/quiz-app/src/pages/JoinLobbyPage.vue
@@ -1,17 +1,24 @@
 <template>
-    <main>
+  <main>
       <div class="content join-lobby">
         <h2>Enter Lobby Code</h2>
-        <form class="join-lobby-form">
-          <input type="text" class="lobby-input input-field" placeholder="Enter lobby code" required/>
+        <form @submit.prevent="submitJoinLobbyForm" class="join-lobby-form">
+          <input v-model="lobbyCode" type="text" class="lobby-input input-field" placeholder="Enter lobby code" required/>
           <button class="button-small" type="submit">Join</button>
         </form>
+        <p v-if="errorMessage">{{ errorMessage }}</p>
       </div>
-    </main>
+  </main>
 </template>
 
 <script>
     export default {
         name: 'JoinLobbyPage',
+        data() {
+        return {
+            lobbyCode: '', 
+            errorMessage: '',
+        };
+    },
     };
 </script>
