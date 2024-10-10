61ca533ca29e8dea2616270a6adabada8c0ed0fa
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-10 11:48:24 +0200

    feat(frontend): only display start lobby button for host

--- front-end/quiz-app/src/pages/LobbyPage.vue
+++ front-end/quiz-app/src/pages/LobbyPage.vue
@@ -5,9 +5,8 @@
             <form class="join-lobby-form" @submit.prevent="startQuiz">
                 <input type="text" class="display-field" :value="lobbyCode" readonly>
                 <p style="margin-bottom: 30px; margin-top: 0px;">Share this room code with your friends!</p>
-                <button class="button-small" type="submit">Start Quiz</button>
-                <button class="button-small red" type="submit">Leave lobby</button>
-                <button class="button-small green" type="submit">Start Quiz</button>
+                <button class="button-small red" type="button">Leave lobby</button>
+                <button v-if="isHost" class="button-small green" type="button">Start Quiz</button>
                 <p>{{ players }}/10 players joined</p>
             </form>
         </div>
@@ -27,9 +26,50 @@
                 required: true
             }
         },
+        data() {
+            return {
+                userId: '',
+                hostId: '',
+                isHost: false,
+            };
+        },
+        async mounted() {
+            await this.checkIfUserIsHost();
+        },
         methods: {
+            async checkIfUserIsHost() {
+                try {
+                    const response = await fetch(`http://localhost:3000/lobby/${this.lobbyCode}`, {
+                        method: 'GET',
+                        credentials: 'include'
+                    });
+                    if (!response.ok) {
+                        throw new Error('Failed to fetch lobby data');
+                    }
+                    const lobbyData = await response.json();
+                    this.hostId = lobbyData.hostId; 
+                    const userResponse = await fetch('http://localhost:3000/check-login', {
+                        method: 'GET',
+                        credentials: 'include'
+                    });
+                    const userData = await userResponse.json();
+                    if (userData.success) {
+                        this.userId = userData.userId;
+                        console.log('Current user ID:', this.userId);
+                    } else {
+                        console.error('User is not authenticated');
+                    }
+                    this.isHost = this.hostId === this.userId; 
+                    console.log('Is host:', this.isHost, 'Host ID:', this.hostId, 'User ID:', this.userId);
+                } catch (error) {
+                    console.error('Error fetching lobby data:', error);
+                }
+            },
             startQuiz(){
-
+                //
+            },
+            leaveLobby(){
+                //
             }
         }
     };
